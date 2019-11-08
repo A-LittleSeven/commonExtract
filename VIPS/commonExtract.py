c84feb3272852b5593cf4a9b4afcaf776f9ffcac
morisama <whatsis.wls@live.com>
2019-11-08 15:23:19 +0800

    add tree

--- VIPS/commonExtract.py
+++ VIPS/commonExtract.py
@@ -9,11 +9,13 @@ from bs4 import BeautifulSoup
 import re
 import requests
 
+# store the prefix
 class Node():
     def __init__(self, pre0, ele0, nlevel):
         self.pre = pre0
         self.ele = ele0
         self.nlevel = nlevel
+
     def __str__(self):
         return "pre:{}, ele:{}, nlevel:{}".format(self.pre, self.ele, self.nlevel)
 
@@ -24,13 +26,13 @@ class commonExtractor():
         
         self.MODE = mode
         self.THRES = thres
-        self.DUBUG = debug
+        self.DUBUG = 1
         # define the ignored tag in html
         self.igonre = ("header", "footer", "script", "style", "a")
         self.ignoreComment = type(_Comment)
         # definr the text containor tags
         self.definite = ("p", "article", "h")
-        self.likelihood = ("p", "article", "ul", "table", "h1")
+        self.likelihood = self.definite  + ("ul", "table", "h1")
         self.ele_splitList = []
 
     def ContentSpliter(self, ghtml):
@@ -42,33 +44,44 @@ class commonExtractor():
         # prettify the input html
         prettydContent = mainContent.prettify(encoding="utf-8")
         tree = etree.HTML(prettydContent)
-        # recursively get child elements
-        self.cutNode(tree)
-        if self.DUBUG:
-            print(self.ele_splitList)
-
+        # recursively get child element
+        self.cutNode(Node(None, tree, 1) ,tree)
+        if self.DUBUG: print(self.ele_splitList)
+        # div = self.miniTree
+        # content = div.toString()
+        
+        
     # filter the useless tag
-    def filter(self, elem_tree, child, nlevel):
-        if elem_tree.tag in self.igonre:
+    def filter(self, node, child, nlevel):
+        if child.tag in self.igonre:
             return False
-        if type(elem_tree) == _Comment:
+        if type(child) == _Comment:
             return False
-        if elem_tree.tag in self.definite:
-            self.ele_splitList.append(Node(child, elem_tree, nlevel))
+        if child.tag in self.definite:
+            self.ele_splitList.append(Node(node, child, nlevel))
             return False
         else:
             return True
 
-    def cutNode(self, elem_tree, nlevel=1):
+    def cutNode(self, node, elem_tree, nlevel = 2):
+        prenode = node
         if elem_tree == []:
             return
         for child in elem_tree.getchildren():
-            if self.filter(elem_tree, child, nlevel):
-                self.ele_splitList.append(Node(elem_tree, child, nlevel))
-                self.cutNode(child, nlevel * 2)
+            if self.filter(prenode, child, nlevel):
+                node = Node(prenode, child, nlevel)
+                self.ele_splitList.append(node)
+                self.cutNode(node, child, nlevel * 2)
             else:
                 continue
-            
+    
+    def findancestor(self):
+        pass
+
+    @staticmethod
+    def readFromUrl(url):
+        pass
+
     @staticmethod
     def readFromFile(filePath):
         with open(filePath, encoding="utf-8") as hfp:
@@ -79,9 +92,8 @@ class commonExtractor():
 if __name__ == "__main__":
 
     # print(_Comment)
-    t = commonExtractor(debug=1)
+    t = commonExtractor(debug=0)
     ghtml = t.readFromFile("baidu.html")
     t.ContentSpliter(ghtml)
-    # print(t.ele_splitList)
 
     
