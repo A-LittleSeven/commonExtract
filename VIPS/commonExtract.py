0c03ac30a3b2327c27799c48265eeb29c48a725d
morisama <whatsis.wls@live.com>
2019-11-08 17:49:14 +0800

    alpha

--- VIPS/commonExtract.py
+++ VIPS/commonExtract.py
@@ -1,5 +1,4 @@
 # coding:utf-8
-
 # __author__ = meow
 # meow meow meow~
 
@@ -11,13 +10,14 @@ import requests
 
 # store the prefix
 class Node():
-    def __init__(self, pre0, ele0, nlevel):
-        self.pre = pre0
-        self.ele = ele0
+    def __init__(self, prefix, element, nlevel):
+        self.pre = prefix
+        self.elem = element
         self.nlevel = nlevel
 
     def __str__(self):
-        return "pre:{}, ele:{}, nlevel:{}".format(self.pre, self.ele, self.nlevel)
+        return "pre:{}, ele:{}, nlevel:{}"\
+            .format(self.pre, self.elem, self.nlevel)
 
 
 class commonExtractor():
@@ -33,7 +33,9 @@ class commonExtractor():
         # definr the text containor tags
         self.definite = ("p", "article", "h")
         self.likelihood = self.definite  + ("ul", "table", "h1")
-        self.ele_splitList = []
+        self.elemTmpList = []
+        self.containor = None
+        
 
     def ContentSpliter(self, ghtml):
 
@@ -46,41 +48,53 @@ class commonExtractor():
         tree = etree.HTML(prettydContent)
         # recursively get child element
         self.cutNode(Node(None, tree, 1) ,tree)
-        if self.DUBUG: print(self.ele_splitList)
-        # div = self.miniTree
-        # content = div.toString()
-        
-        
-    # filter the useless tag
-    def filter(self, node, child, nlevel):
-        if child.tag in self.igonre:
-            return False
-        if type(child) == _Comment:
-            return False
-        if child.tag in self.definite:
-            self.ele_splitList.append(Node(node, child, nlevel))
-            return False
-        else:
-            return True
+        if self.DUBUG: print(etree.tostring(self.containor.elem))
 
-    def cutNode(self, node, elem_tree, nlevel = 2):
+
+
+    # define the stop rules
+    def filter(self, node, child, nlevel):
+        if child.tag in self.igonre: return False
+        if type(child) == _Comment: return False
+        if child.tag in self.definite:
+            self.findancestor(node)
+            # self.elemTmpList.append(Node(node, child, nlevel))
+            return False
+        else: return True
+
+
+    def cutNode(self,  node , elem_tree, nlevel = 2):
         prenode = node
         if elem_tree == []:
             return
         for child in elem_tree.getchildren():
             if self.filter(prenode, child, nlevel):
                 node = Node(prenode, child, nlevel)
-                self.ele_splitList.append(node)
+                self.elemTmpList.append(node)
                 self.cutNode(node, child, nlevel * 2)
             else:
                 continue
     
-    def findancestor(self):
-        pass
+    def findancestor(self, node):
+        if self.containor == None:
+            self.containor = node.pre
+        elif node.pre == self.containor:
+            return
+        else:
+            self.containor = self.containor.pre
+            return self.findancestor(node)
+        
 
     @staticmethod
     def readFromUrl(url):
-        pass
+        header = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
+                (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36'
+        }
+        res = requests.get(url, headers=header)
+        res.encoding = res.apparent_encoding
+        content = res.content
+        return content
 
     @staticmethod
     def readFromFile(filePath):
@@ -92,8 +106,6 @@ class commonExtractor():
 if __name__ == "__main__":
 
     # print(_Comment)
-    t = commonExtractor(debug=0)
-    ghtml = t.readFromFile("baidu.html")
+    t = commonExtractor(debug=1)
+    ghtml = t.readFromUrl("https://www.cnblogs.com/xieqiankun/p/generalnewsextractor.html")
     t.ContentSpliter(ghtml)
-
-    
