3b85b5454e81c2f66ac917ec79e9c5cbeecf3be1
MoriSama <whatsis.wls@live.com>
2019-11-13 21:10:39 +0800

    [add] picture link

--- VIPS/commonExtract.py
+++ VIPS/commonExtract.py
@@ -13,6 +13,10 @@ class Node():
         self.elem = element
         self.nlevel = nlevel
 
+    def cut(self, node):
+        del node
+        return 0
+
     def __str__(self):
         return "pre:{}, ele:{}, nlevel:{}"\
             .format(self.pre, self.elem, self.nlevel)
@@ -20,51 +24,67 @@ class Node():
 
 class commonExtractor():
 
-    def __init__(self, mode='definite', debug=0):
+    def __init__(self, debug=1, image=False, thres=20):
         
-        self.MODE = mode
-        self.DUBUG = 1
-        # define the ignored tag in html
-        self.igonre = ("header", "footer", "script", "style", "a", "form", "nav", "head")
-        self.ignoreComment = type(_Comment)
-        # definr the text containor tags
-        self.definite = ("p","blockquote")
-        self.likelihood = self.definite  + ("ul", "table")
-        self.elemTmpList = []
+        self.DUBUG = debug
+        self.IMAGE = image
+        # TODO: 对标签内文字做阈值处理
+        self.THRES = thres
+        self.definite = ("p", "blockquote")
         self.containor = None
 
-    #  preprocess the dom tree
-    def cutUseless(self, elem_tree):
-        for child in elem_tree.getchildren():
-            if child.tag in self.igonre or type(child) == _Comment:
-                child.getparent().remove(child)
-            self.cutUseless(child)
-        return elem_tree
-        
-    def ContentSpliter(self, ghtml):
+    def readUrl(self):
+        header = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
+        (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36'
+        }
+        try:
+            res = requests.get(self.url, headers=header)
+            content = res.text
+            return content
+        except Exception as e:
+            print(e)
 
-        ele_parser = []
-        tree = self.cutUseless(etree.HTML(ghtml))
+    #  preprocess the dom tree
+    def processUseless(self, elem_tree):
+        ignore = ("header", "footer", "script",
+                  "style", "a", "form", "nav", "head")
+        for child in elem_tree.getchildren():
+            if child.tag in ignore or type(child) == _Comment:
+                child.getparent().remove(child)
+            self.processUseless(child)
+        return elem_tree
+    
+    def processImgs(self, content):
+        reIMG = re.compile(r'<img[\s\S]*?src=[\'|"]([\s\S]*?)[\'|"][\s\S]*?>')
+        content = reIMG.sub(r'{\1}', content)
+        return content
+        
+    def ContentSpliter(self, ghtml): 
+        tree = self.processUseless(etree.HTML(ghtml))
         # recursively get child element
         self.cutNode(Node(None, tree, 1) ,tree)
-        # if self.DUBUG: print(etree.tostring(self.containor.elem, encoding="utf-8"))
-        Hcontent = str(etree.tostring(self.containor.elem, encoding="utf-8"), encoding="utf-8")
-        reTAG  = r'<[\s\S]*?>|[ \t\r\f\v]'
+        elemContent = etree.tostring(self.containor.elem, encoding=str)
+        Hcontent = str(elemContent)
+        if self.IMAGE:
+            Hcontent = self.processImgs(Hcontent)
+        reTAG = r'<[\s\S]*?>|[ \t\r\f\v]|&#13;|&gt;'
         filterdContent = re.sub(reTAG, "", Hcontent)
-        content = "".join(re.findall("\s\S+", filterdContent))
-        if self.DUBUG: print(content)
-        with open("dede.txt", 'w') as f:
-            f.write(Hcontent)
+        mainContent = "".join(re.findall("\s\S+", filterdContent))
+        if self.DUBUG:
+            print(mainContent)
+            with open("mainContent.txt", 'w') as f:
+                f.write(mainContent)
+        return mainContent
 
     # define the search rule
     def dynamicfind(self, node, child, nlevel):
         if child.tag in self.definite:
             self.findancestor(node)
-            # self.elemTmpList.append(Node(node, child, nlevel))
             return False
         else: return True
 
-    def cutNode(self,  node , elem_tree, nlevel = 2):
+    def cutNode(self, node, elem_tree, nlevel = 2):
         prenode = node
         if elem_tree == []:
             return
@@ -84,21 +104,8 @@ class commonExtractor():
         else:
             if self.containor.nlevel >= node.nlevel:
                 self.containor = self.containor.pre
-            else:
-                node = node.pre
+            else: node = node.pre
             return self.findancestor(node)
-        
-
-    @staticmethod
-    def readFromUrl(url):
-        header = {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
-                (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36'
-        }
-        res = requests.get(url, headers=header)
-        res.encoding = res.apparent_encoding
-        content = res.content
-        return content
 
     @staticmethod
     def readFromFile(filePath):
@@ -108,9 +115,10 @@ class commonExtractor():
 
 
 if __name__ == "__main__":
-
-    # print(_Comment)
-    t = commonExtractor(debug=1)
-    # ghtml = t.readFromUrl("https://rpy2.readthedocs.io/en/version_2.8.x/robjects_rpackages.html#importing-arbitrary-r-code-as-a-package")
-    ghtml = t.readFromFile("testDynamic.html")
+    
+    # http://www.sohu.com/a/353449767_267106?g=0%253Fcode=a315343389f3a899c29ceaed4215ceb0&spm=smpc.home.top-news1.1.1573626200338DhoZ4ic&_f=index_cpc_0
+    t = commonExtractor(debug=1, image=True)
+    ghtml = t.readFromUrl(
+        "https://www.zybuluo.com/Alston/note/778377")
+    # ghtml = t.readFromFile("testDynamic.html")
     t.ContentSpliter(ghtml)
